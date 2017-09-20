e68dc7fe6556609ea4f848a2dbb6ad08c308612b
= <webdev.jameslemire@gmail.com>
2017-09-20 15:00:18 -0600

    JL gh-pages - V2

--- js/app.js
+++ js/app.js
@@ -1,1 +1,21 @@
-angular.module('userProfiles', []);
+angular.module('myApp', ['ui.router']).config( function( $stateProvider, $urlRouterProvider ) {
+  $stateProvider
+    .state('home', {
+      url: '/',
+      templateUrl: 'js/home/homeTmpl.html',
+      controller: 'homeCtrl'
+    })
+    .state('products', {
+      url: '/products/:id',
+      templateUrl: 'js/products/productsTmpl.html',
+      controller: 'productsCtrl'
+    })
+    .state('settings', {
+      url: '/settings',
+      templateUrl: 'js/settings/settingsTmpl.html',
+      controller: 'settingsCtrl'
+    });
+
+  $urlRouterProvider
+    .otherwise('/');
+});
--- js/service.js
+++
@@ -1,8 +0,0 @@
-angular.module('userProfiles').service('mainService', function( $http ) {
-  this.getUsers = function() {
-    return $http({
-      method: 'GET',
-      url: 'https://reqres.in/api/users?page=1'
-    });
-  };
-});
